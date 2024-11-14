e685e203c4e40723bf697ef2e51493699fce8f94
mrjayrai <raijai127@gmail.com>
2024-11-14 20:46:58 +0530

    Boiler Plate for Road Ready

---
+++ RoadReady/src/main/java/com/hexaware/roadready/RoadReadyApplication.java
@@ -0,0 +1,13 @@
+package com.hexaware.roadready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadReadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadReadyApplication.class, args);
+	}
+
+}
---
+++ RoadReady/src/main/java/com/hexaware/roadready/repositories/RoadReadyRepository.java
@@ -0,0 +1,5 @@
+package com.hexaware.roadready.repositories;
+
+public interface RoadReadyRepository   {
+
+}
